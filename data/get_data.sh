934dc4993ea835f4af6e08128f72a88a7e167671
luofan18 <luofan18@126.com>
2017-03-06 03:05:58 +0000

    add script to download dataset

---
+++ data/get_data.sh
@@ -0,0 +1,5 @@
+mkdir image/
+curl http://www.robots.ox.ac.uk/~vgg/data/oxbuildings/oxbuild_images.tgz | tar xz -C image/
+
+mkdir gt_files_170407/
+curl http://www.robots.ox.ac.uk/~vgg/data/oxbuildings/gt_files_170407.tgz | tar xz -C gt_files_170407/
